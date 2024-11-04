7860d1ac2689fdef9c374282cf911c0afabb4fc9
JustArmaan <singharmaanbrar@gmail.com>
2024-11-03 17:11:52 -0800

    feature: complete mp4 upload

---
+++ src/app/mp4-upload/page.tsx
@@ -0,0 +1,10 @@
+import YouTubeUploadForm from "@/components/mp4ConvertForm";
+
+export default function YouTubeUploadPage() {
+  return (
+    <div className="container mx-auto p-8">
+      <h1 className="text-2xl font-bold mb-6">YouTube to MP3 Upload</h1>
+      <YouTubeUploadForm />
+    </div>
+  );
+}
